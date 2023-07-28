7bb1c93e5a1c6e8a8116049ef5e6daf3d1f20541
Borito <behrensricardo@gmail.com>
2023-07-28 13:43:36 -0600

    added the new classes to each hour block so that as the clock moves forward, it identifies in which time we are and it modifies each class to either past, present or future. Additionally, added eventListener to the save button so that they can store information locally on each hour-block

--- assets/js/script.js
+++ assets/js/script.js
@@ -25,8 +25,7 @@
 // Wrap all code that interacts with the DOM in a call to jQuery to ensure that
 // the code isn't run until the browser has finished rendering all the elements
 // in the html.
-$(function () {
-});
+
 // TODO: Add a listener for click events on the save button. This code should
 // use the id in the containing time-block as a key to save the user input in
 // local storage. HINT: What does `this` reference in the click listener
@@ -39,19 +38,76 @@ $(function () {
 // attribute of each time-block be used to conditionally add or remove the
 // past, present, and future classes? How can Day.js be used to get the
 // current hour in 24-hour time?
+// var hour = today.format('h')
+
+var today = dayjs();
 
 function updateTime() {
-  var today = dayjs();
   var currentDayElement = document.getElementById('currentDay');
   currentDayElement.textContent = today.format('dddd, MMMM D YYYY, h:mm:ss a');
 }
-
 // Call the updateCurrentDay function every second (1000 milliseconds)
 setInterval(updateTime, 1000);
 
+var timeBlocks = document.querySelectorAll('.time-block');
+var hour = today.format('H')
 
+timeBlocks.forEach(block => {
+  const id = block.id;
+  const lastTwoCharacters = id.slice(-2); // Using slice method
 
-  //
+  // Use the last two characters in the div's id to determine what's past, present and future
+  timeBlocks.forEach(block => {
+    const id = block.id;
+    const lastTwoCharacters = id.slice(-2); // Using slice method
+
+    if (parseInt(lastTwoCharacters) < parseInt(today.format('H'))) {
+      block.classList.remove('present', 'future'); // Remove present and future classes
+      block.classList.add('past'); // Add the 'past' class
+    } else if (parseInt(lastTwoCharacters) > parseInt(today.format('H'))) {
+      block.classList.remove('past', 'present'); // Remove past and future classes
+      block.classList.add('future'); // Add the 'present' class
+    } else {
+      block.classList.remove('past', 'future'); // Remove past and present classes
+      block.classList.add('present'); // Add the 'future' class
+    }
+  });
+});
+
+// Function to save the user's input to localStorage
+function saveDataToLocalStorage() {
+  const timeBlocks = document.querySelectorAll('.time-block');
+  timeBlocks.forEach(block => {
+    const id = block.id;
+    const description = block.querySelector('.description').value;
+    localStorage.setItem(id, description);
+  });
+}
+
+// Function to load the saved data from localStorage and display it in the scheduler
+function loadSavedDataFromLocalStorage() {
+  const timeBlocks = document.querySelectorAll('.time-block');
+  timeBlocks.forEach(block => {
+    const id = block.id;
+    const description = localStorage.getItem(id);
+    if (description) {
+      block.querySelector('.description').value = description;
+    }
+  });
+}
+
+// Call the functions to load the saved data and update the scheduler
+loadSavedDataFromLocalStorage();
+updateTime();
+
+// Add event listeners to the save buttons
+const saveButtons = document.querySelectorAll('.saveBtn');
+saveButtons.forEach(button => {
+  button.addEventListener('click', () => {
+    saveDataToLocalStorage();
+  });
+});
+
   // TODO: Add code to get any user input that was saved in localStorage and set
   // the values of the corresponding textarea elements. HINT: How can the id
   // attribute of each time-block be used to do this?
