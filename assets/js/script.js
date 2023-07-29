9e43aeef625139ef8fd84b862aababb110c416b5
Borito <behrensricardo@gmail.com>
2023-07-29 11:26:28 -0600

    Added new code to save items individually by hour block using event.target

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,19 +1,3 @@
-
-// TODO: Add a listener for click events on the save button. This code should
-// use the id in the containing time-block as a key to save the user input in
-// local storage. HINT: What does `this` reference in the click listener
-// function? How can DOM traversal be used to get the "hour-x" id of the
-// time-block containing the button that was clicked? How might the id be
-// useful when saving the description in local storage?
-//
-// TODO: Add code to apply the past, present, or future class to each time
-// block by comparing the id to the current hour. HINTS: How can the id
-// attribute of each time-block be used to conditionally add or remove the
-// past, present, and future classes? How can Day.js be used to get the
-// current hour in 24-hour time?
-// var hour = today.format('h')
-
-
 // Define a variable 'today' to store the current date and time using the dayjs library.
 var today = dayjs();
 
@@ -38,8 +22,8 @@ $(document).ready(function () {
 
   // Loop through each element with class 'time-block' to determine if it's past, present, or future.
   timeBlocks.forEach(block => {
-    const id = block.id; // Get the ID of the current time block.
-    const lastTwoCharacters = id.slice(-2); // Extract the last two characters from the ID.
+    var id = block.id; // Get the ID of the current time block.
+    var lastTwoCharacters = id.slice(-2); // Extract the last two characters from the ID.
 
     // Check if the time represented by the last two characters is less than the current hour.
     if (parseInt(lastTwoCharacters) < parseInt(today.format('H'))) {
@@ -55,21 +39,16 @@ $(document).ready(function () {
   });
 
   // Function to save the user's input from the 'description' field to localStorage.
-  function saveDataToLocalStorage() {
-    const timeBlocks = document.querySelectorAll('.time-block'); // Get all elements with class 'time-block' again.
-    timeBlocks.forEach(block => {
-      const id = block.id; // Get the ID of the current time block.
-      const description = block.querySelector('.description').value; // Get the value of the 'description' field within the current time block.
-      localStorage.setItem(id, description); // Save the description value in localStorage with the time block ID as the key.
-    });
+  function saveDataToLocalStorage(description, id) {
+    localStorage.setItem(id, description); // Save the description value in localStorage with the time block ID as the key.
   }
 
   // Function to load the saved data from localStorage and display it in the 'description' field for each time block.
   function loadSavedDataFromLocalStorage() {
-    const timeBlocks = document.querySelectorAll('.time-block'); // Get all elements with class 'time-block'.
+    var timeBlocks = document.querySelectorAll('.time-block'); // Get all elements with class 'time-block'.
     timeBlocks.forEach(block => {
-      const id = block.id; // Get the ID of the current time block.
-      const description = localStorage.getItem(id); // Get the saved description from localStorage using the time block ID as the key.
+      var id = block.id; // Get the ID of the current time block.
+      var description = localStorage.getItem(id); // Get the saved description from localStorage using the time block ID as the key.
       if (description) {
         block.querySelector('.description').value = description; // If there's a description, set it as the value of the 'description' field within the time block.
       }
@@ -83,19 +62,22 @@ $(document).ready(function () {
   updateTime();
 
   // Add event listeners to all elements with class 'saveBtn'.
-  const saveButtons = document.querySelectorAll('.saveBtn');
+  var saveButtons = document.querySelectorAll('.saveBtn');
   saveButtons.forEach(button => {
     // Attach a click event listener to each 'saveBtn' element.
-    button.addEventListener('click', () => {
-      saveDataToLocalStorage(); // Call the function to save the user's input to localStorage when the button is clicked.
+    button.addEventListener('click', (event) => {
+      var descriptionField = event.target.parentElement.querySelector('.description');
+      var description = descriptionField.value;
+      var id = event.target.parentElement.id;
+      saveDataToLocalStorage(description, id); // Call the function to save the user's input to localStorage when the button is clicked.
       showMessage(); // Call the function to show the message.
     });
   });
 
   // Function to show the message container when the save button is clicked.
   function showMessage() {
-    const messageElement = document.getElementById('message');
-    const messageTextElement = document.getElementById('messageText');
+    var messageElement = document.getElementById('message');
+    var messageTextElement = document.getElementById('messageText');
     messageTextElement.textContent = "Appointment Added to localStorage ✔️"; // Set the message text
     messageElement.style.display = 'block'; // Set the display property to 'block' to show the message.
   
@@ -104,17 +86,5 @@ $(document).ready(function () {
       messageElement.style.display = 'none'; // Set the display property to 'none' to hide the message after the delay.
     }, 3000); // Adjust the time (in milliseconds) as per your preference.
   }
-
-
-  // Alternatively, use jQuery to add a click event listener to all elements with class 'saveBtn'.
-  $('.saveBtn').on('click', function () {
-    saveDataToLocalStorage(); // Call the function to save the user's input to localStorage when the button is clicked.
-
-  });
 });
 
-// TODO: Add code to get any user input that was saved in localStorage and set
-  // the values of the corresponding textarea elements. HINT: How can the id
-  // attribute of each time-block be used to do this?
-  //
-  // TODO: Add code to display the current date in the header of the page.
